0391f448b72cfc66140b2c4ceb83e19a064b9dd9
swayam rawat <swaymrawat862@gmail.com>
2026-09-17 23:54:22 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.82%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i;
+        for(i=1;i<arr.size();i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                return i;
+            }}
+            
+                return -1;
+            
+        }
+    
+};
